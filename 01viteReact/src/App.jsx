d5db3c7ddededd4f84f56d9308ab9c09dbb6dbb3
VarreyShivaGaneshBhanuPrathyush <prathyush.varrey123@gmail.com>
2023-10-23 18:03:45 +0530

    Added and edited Chai.jsx and App.jsx

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-
+import Chai from "./Chai"
 
 function App() {
 
@@ -7,7 +7,8 @@ function App() {
     <>
           <h1>
               Shiva Ganesh Bhani Prathyush Varrey
-          </h1>
+            </h1>
+            <Chai/>
     </>
   )
 }
