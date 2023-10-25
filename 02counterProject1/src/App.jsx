2ac746331150ffd764b4b5bd0de13af4b2be6409
VarreyShivaGaneshBhanuPrathyush <prathyush.varrey123@gmail.com>
2023-10-25 18:15:12 +0530

    Added a New File 02counterProject1 and Modified README.md file

---
+++ 02counterProject1/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+     let [counter, setCounter] = useState(1);
+     // counter => A variable that holds the value  
+     /*
+          setCounter => used to Update the value and where 
+          ever it is on the UI 
+          
+          the work of the useState is to Update the Values
+          that are on the UI
+     */
+     
+     const addValue = () => {
+          setCounter(counter + 1)
+     }
+
+     const removeValue = () => {
+          setCounter(counter -1)
+     }
+
+     const doubleValue = () => {
+               setCounter(counter * 2)
+     }
+
+     const zeroValue = () =>{setCounter(0)}
+
+  return (
+    <>
+            <h1>Counter Practice</h1>
+            <p>React 1st Lectur</p>
+            <h4>Counter Value : {counter}</h4>
+
+            <button onClick={addValue}>Add Value By 1 : </button> <button>{counter}</button> <br /> 
+            <button onClick={removeValue}>Remove Value By 1 : </button> <button>{counter}</button> <br />
+            <button onClick={doubleValue}>Multiply Value By 2 : </button> <button>{counter}</button> <br />
+            <button onClick={zeroValue}>Set Value To 0 : </button> <button>{counter}</button>
+    </>
+  )
+}
+
+export default App
